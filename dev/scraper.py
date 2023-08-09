de5da3c6761741452c2add40cdee380409d8b5f4
Christopher Peret <cperet89@gmail.com>
2023-08-09 11:43:15 -0700

    scraper.py init

---
+++ dev/scraper.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+def get_mtime(file_path):
+    """
+    Get the last modified time of a file.
+    """
+    file_stat = os.stat(file_path)
+    return time.ctime(file_stat.st_mtime)
+
+def list_files_with_mtime(directory):
+    """
+    List all files in a directory with their last modified time.
+    """
+    for foldername, subfolders, filenames in os.walk(directory):
+        for filename in filenames:
+            full_path = os.path.join(foldername, filename)
+            print(f"{full_path}: Last modified on {get_mtime(full_path)}")
+
+if __name__ == "__main__":
+    dir_path = input("Enter the path of the directory: ")
+    list_files_with_mtime(dir_path)
